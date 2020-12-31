9c0eff0020d696733f5cf5975d7ac020c0831de2
Andreas <sueppi@gmx.ch>
2020-12-31 10:01:49 +0100

    Start shell-script added

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Here is where I set a bunch of environment variables that are
+# used by the process invoked below...
+
+# Now I want to invoke the process in the background and redirect
+# all of the output to a log file.
+nohup python3 websocket.py > logfile-massagedevice.out 2>&1 &
+
+# Finally, I record the PID of the process before the script exits.
+echo $! > proc.pid
