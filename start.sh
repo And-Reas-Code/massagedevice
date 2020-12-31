719c90796e15767a06f4d3f6a260bad95eed7568
Andreas <sueppi@gmx.ch>
2020-12-31 10:37:37 +0100

    NoHup settings

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 
 # Now I want to invoke the process in the background and redirect
 # all of the output to a log file.
-nohup sudo python3 websocket.py > /home/pi/logfile-massagedevice.out 2>&1 &
+sudo nohup python3 websocket.py > /home/pi/logfile-massagedevice.out 2>&1 &
 
 # Finally, I record the PID of the process before the script exits.
-echo $! > proc.pid
+echo $! > /home/pi/proc.pid
