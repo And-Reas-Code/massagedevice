5dc583e85e8d30174e6f4cc78da9ed9bfcdf10fc
Andreas <sueppi@gmx.ch>
2025-01-04 07:26:01 +0100

    Service enabled

--- service.sh
+++ service.sh
@@ -5,7 +5,7 @@
 
 # Now I want to invoke the process in the background and redirect
 # all of the output to a log file.
-sudo nohup python3 websocket.py > /home/andreas/logfile-massagedevice.out 2>&1 &
+python3 /home/andreas/massagedevice/websocket.py > /home/andreas/logfile-massagedevice.out 2>&1 &
 
 # Finally, I record the PID of the process before the script exits.
 echo $! > /home/andreas/proc.pid
